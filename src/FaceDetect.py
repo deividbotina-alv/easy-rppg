6bbc65e9fc23b7c42aeb7350a52efb2f2c9b93c3
HenryAreiza <53319367+HenryAreiza@users.noreply.github.com>
2023-05-25 18:28:43 +0200

    [ADD] First version of FaceDetect.py
    This version was fully created by using ChatGPT. The next step is to adapt it to match with the classes defined in the planning of the project.

---
+++ src/FaceDetect.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 25 17:54:49 2023
+
+@author: HenRick
+"""
+
+import cv2
+
+# Load the pre-trained face detection model
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# Open the video capture
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    # Read the current frame from the video capture
+    ret, frame = video_capture.read()
+
+    # Convert the frame to grayscale for face detection
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Perform face detection
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    # Draw rectangles around the detected faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Display the resulting frame
+    cv2.imshow('Face Detection', frame)
+
+    # Exit the loop if 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the video capture and close the window
+video_capture.release()
+cv2.destroyAllWindows()
